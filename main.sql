381d68b028c5abea851780a677b8c88e68222d90
peterrobert <pwambui93@gmail.com>
2020-04-16 21:42:51 +0300

    select basics exercise done

--- main.sql
+++ main.sql
@@ -1,3 +1,11 @@
 -- exercise one 
 SELECT population FROM world
   WHERE name = 'Germany'
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+-- exercise one  
